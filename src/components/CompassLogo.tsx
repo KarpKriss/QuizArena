756d372c4704d5cdefe4fe90d50d9c5127773bbf
KarpKriss <kmachalica000@gmail.com>
2026-08-11 23:19:29 +0200

    fix: render compass logo from verified PNG data

--- src/components/CompassLogo.tsx
+++ src/components/CompassLogo.tsx
@@ -1,4 +1,10 @@
-import compassLogo from '../assets/quiz-arena-compass.png'
+import part1 from '../assets/compass-data/part1'
+import part2 from '../assets/compass-data/part2'
+import part3 from '../assets/compass-data/part3'
+import part4 from '../assets/compass-data/part4'
+import part5 from '../assets/compass-data/part5'
+
+const compassLogo = `data:image/png;base64,${part1}${part2}${part3}${part4}${part5}`
 
 export default function CompassLogo({ compact = false }: { compact?: boolean }) {
   return (
